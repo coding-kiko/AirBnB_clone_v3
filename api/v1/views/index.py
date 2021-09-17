caf339ee8a49c101762ac104069a40ada650d302
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-17 18:22:42 -0300

    task 7

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,7 +12,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     '''returns request status'''
     jsn = {
@@ -21,7 +21,7 @@ def status():
     return (jsonify(**jsn))
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def n_of_inst():
     '''retrieves n of each instance'''
     classes = {"amenities": Amenity, "cities": City, "places": Place,
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ handles restful api actions """
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 
@@ -22,6 +22,24 @@ def get_states(state_id=None):
     return (jsonify(state_list))
 
 
-#@app_views.route('/states/<state_id>', methods=['DELETE'])
-#def delete_state_id(state_id):
-#    """Retrieves the list of all State objects"""
+@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state_id(state_id):
+    """ Deletes an state obj based on its id """
+    f = False
+    d = {}
+    for state in storage.all("State").values():
+        if state.id == state_id:
+            state.delete()
+            f = True
+    if f:
+        return (jsonify(**d))
+    abort(404)
+
+@app_views.route('/states/<string:state_id>', methods=['POST'], strict_slashes=False)
+def create_state(state_id):
+    """ creates an state obj based on its id """
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """ updates an state obj based on its id """
