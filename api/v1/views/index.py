d0990ea4a53e9463c64d46c2ffb13ff145cc0f81
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-17 12:11:50 -0300

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,15 @@
 ''' Index '''
 
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
 from flask import Flask, jsonify
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models import storage
+from models.user import User
+
 
 
 @app_views.route('/status')
@@ -13,3 +20,13 @@ def status():
         "status": "OK"
         }
     return (jsonify(**jsn))
+
+
+@app_views.route('/api/v1/stats')
+def n_of_inst():
+    '''retrieves n of each instance'''
+    classes = {"amenity": Amenity, "city": City, "place": Place,
+               "review": Review, "state": State, "user": User}
+    for cls_str, cls in classes.items():
+        classes[cls_str] = storage.count(cls)
+    return (jsonify(**classes))
