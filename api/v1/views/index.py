5c7a989291f8162680009c0fa982b632bfad7cea
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-17 11:58:39 -0300

    s

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,13 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     '''returns request status'''
-    jsn = {"status": "OK"}
+    jsn = {
+        "status": "OK"
+        }
     return (jsonify(**jsn))
