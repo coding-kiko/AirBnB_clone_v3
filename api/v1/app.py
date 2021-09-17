1794596d426e79133e40b7069f99189c5a360c38
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-17 12:57:52 -0300

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,17 +2,25 @@
 """ app.py """
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def remove_sesh(self):
     '''remove the current SQLAlchemy Session after request'''
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found():
+    '''404 page not found handler'''
+    err = {"error": "Not found"}
+    return (jsonify(**err))
 
 if __name__ == "__main__":
     h = getenv('HBNB_API_HOST')
