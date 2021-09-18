ff06863f4e00cb2693d89896d90e96fcd18fcb07
Ksualboy <2758@holbertonschool.com>
2021-09-18 22:01:20 +0000

    fixing get

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ handles restful api actions """
 
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -10,17 +10,17 @@ from models.state import State
 @app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
 def get_states(state_id=None):
     """Retrieves the list of all State objects"""
-    state_list = []
     if (state_id):
         for state in storage.all("State").values():
             if state.id == state_id:
-                state_list.append(state.to_dict())
-        if (len(state_list) == 0):
-            abort(404)
+                return (jsonify(state.to_dict()))
+        abort(404)
+
     else:
+        state_list = []
         for state in storage.all("State").values():
             state_list.append(state.to_dict())
-    return (jsonify(state_list))
+        return (jsonify(state_list))
 
 
 @app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
@@ -51,7 +51,7 @@ def create_state():
     storage.new(obj)
     storage.save()
 
-    return make_response(jsonify(obj.to_dict()), 201)
+    return (jsonify(obj.to_dict()), 201)
 
 
 @app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
