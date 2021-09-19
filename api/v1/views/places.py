4ea8a4361ea6fa49581752cd7738c680c87d1292
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-19 19:40:52 -0300

    import error

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ handles restful api actions """
 
-from _typeshed import StrPath
 from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
