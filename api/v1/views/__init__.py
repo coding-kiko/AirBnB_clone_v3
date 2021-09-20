0c06f22ee51c22038980a7dbfe335f68548b7d5f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-20 12:01:28 -0300

    pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,6 @@
 """ Init """
 
 from flask import Flask, Blueprint
-
-app = Flask(__name__)
-app_views = Blueprint("app_views", __name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -14,3 +9,7 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+app = Flask(__name__)
+app_views = Blueprint("app_views", __name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
